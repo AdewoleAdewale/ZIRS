cf6347a60e7443926ed83fa0d284baccc3f67e9d
Adewole Adewale <sundayadewole1997@gmail.com>
2026-09-26 17:53:39 +0100

    final update

---
+++ ZamfaraIRS.Android.TrenditBinding/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("ZamfaraIRS Android Trendit Binding")]
+[assembly: AssemblyDescription("Trendit base SDK Xamarin.Android binding")]
+[assembly: AssemblyCompany("ZamfaraIRS")]
+[assembly: AssemblyProduct("ZamfaraIRS")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- ZamfaraIRS.Android/Services/TrenditPrinterService.cs
+++ ZamfaraIRS.Android/Services/TrenditPrinterService.cs
@@ -1,13 +1,13 @@
 ﻿using System;
-using System.Text;
 using System.Threading.Tasks;
 using Android.OS;
+using Com.Trendit.Basesdk;
+using Com.Trendit.Basesdk.Device.Printer;
+using Com.Trendit.Basesdk.Device.Printer.Format;
 using Xamarin.Forms;
 using ZamfaraIRS.Droid.Services;
 using ZamfaraIRS.Models;
 using ZamfaraIRS.Services;
-// Add your Trendit SDK binding namespace here (e.g., using Com.Trendit.Basesdk.Device.Pos;)
-
 [assembly: Dependency(typeof(TrenditPrinterService))]
 namespace ZamfaraIRS.Droid.Services
 {
@@ -36,10 +36,10 @@ namespace ZamfaraIRS.Droid.Services
 
                 // Configure default format
                 var format = printer.DefaultTextFormat;
-                format.IsBold = true;
+                format.Bold = true;
 
                 printer.PrintText(format, "ZAMFARA I.R.S.\n");
-                format.IsBold = false;
+                format.Bold = false;
                 printer.PrintText(format, "Trendit S680 Printer Test OK!\n\n\n");
 
                 printer.StartPrint(new PrintListener(tcs));
@@ -68,13 +68,13 @@ namespace ZamfaraIRS.Droid.Services
                 var format = printer.DefaultTextFormat;
 
                 // 4. Queue your lines
-                format.IsBold = true;
-                format.Align = 1; // Center
+                format.Bold = true;
+                format.Align = PrintAlign.FormatAlignCenter;
                 printer.PrintText(format, $"{receipt.StoreName}\n");
                 printer.PrintText(format, $"{receipt.StoreSubTitle}\n");
 
-                format.IsBold = false;
-                format.Align = 0; // Left
+                format.Bold = false;
+                format.Align = PrintAlign.FormatAlignLeft;
                 printer.PrintText(format, $"Ref: {receipt.ReceiptNumber}\n");
                 printer.PrintText(format, $"Date: {receipt.PrintDate:dd-MMM-yyyy HH:mm}\n");
                 printer.PrintText(format, "--------------------------------\n");
@@ -89,17 +89,20 @@ namespace ZamfaraIRS.Droid.Services
                 }
 
                 printer.PrintText(format, "--------------------------------\n");
-                format.IsBold = true;
+                format.Bold = true;
                 printer.PrintMultiText(format, "TOTAL AMOUNT:", $"NGN {receipt.TotalAmount:N2}");
-                format.IsBold = false;
+                format.Bold = false;
 
                 printer.PrintText(format, "--------------------------------\n");
 
                 // Barcode / QR Code
                 if (!string.IsNullOrEmpty(receipt.BarcodeLabel))
                 {
-                    format.Align = 1; // Center
-                    printer.PrintQrCode(receipt.BarcodeLabel, 300, 300);
+                    QrCodeFormat qrFormat = printer.DefaultQrCodeFormat;
+                    qrFormat.Align = PrintAlign.FormatAlignCenter;
+                    qrFormat.Width = 300;
+                    qrFormat.Height = 300;
+                    printer.PrintQrCode(qrFormat, receipt.BarcodeLabel);
                     printer.PrintText(format, "\n");
                 }
 
@@ -117,15 +120,14 @@ namespace ZamfaraIRS.Droid.Services
             return await tcs.Task;
         }
 
-        // Native callback listener for Trendit SDK
-        private class PrintListener : Java.Lang.Object, IPrintListener // Replace IPrintListener with exact SDK interface
+        private sealed class PrintListener : OnPrintTaskListener
         {
             private readonly TaskCompletionSource<bool> _tcs;
+
             public PrintListener(TaskCompletionSource<bool> tcs) { _tcs = tcs; }
 
-            public void OnPrintResult(int resultCode)
+            public override void OnPrintResult(int resultCode)
             {
-                // 0 means success; any other code means an error
                 _tcs.TrySetResult(resultCode == 0);
             }
         }
